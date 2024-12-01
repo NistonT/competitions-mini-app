4798f657eed5e3c6b3b1ef99c5c402b6fdc8be46
NistonN <nisprogrammer@gmail.com>
2024-12-01 22:07:36 +0300

    web-socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+// import QRCode from 'react-qr-code'
+import useWebSocket from "react-use-websocket";
+
 const tg = window.Telegram.WebApp;
 
 function App() {
-	return <>{tg.initDataUnsafe.user?.username}</>;
+	const TOKEN = "ABCD";
+	// const initData = tg.initData;
+
+	const { lastJsonMessage, sendJsonMessage } = useWebSocket(
+		`wss://competitions.webository.ru/connect_user?token=${TOKEN}`
+	);
+
+	sendJsonMessage({
+		event: "USERS:GET_ME",
+		data: null,
+	});
+
+	useEffect(() => {
+		if (lastJsonMessage === null) return;
+
+		switch (lastJsonMessage.event) {
+			case "USERS:GET_ME:RESULT":
+				console.log(lastJsonMessage.data);
+		}
+	}, [lastJsonMessage]);
+
+	return (
+		<>
+			{tg.initDataUnsafe.user?.username}
+			{/* <QRCode value={} /> */}
+		</>
+	);
 }
 
 export default App;
